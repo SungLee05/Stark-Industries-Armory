43ed312ca93e5c659d2c843f98e8e341f41dfb33
Sung Lee <sungyonglee414@gmail.com>
2021-05-01 12:49:33 -0700

    feat(FE): stripe loading added to client.

--- client/index.js
+++ client/index.js
@@ -5,14 +5,20 @@ import {Router} from 'react-router-dom'
 import history from './history'
 import store from './store'
 import App from './app'
+import {loadStripe} from '@stripe/stripe-js'
+import {Element} from '@stripe/react-stripe-js'
 
 // establishes socket connection
 import './socket'
 
+const stripe = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY)
+
 ReactDOM.render(
   <Provider store={store}>
     <Router history={history}>
-      <App />
+      <Element stripe={stripe}>
+        <App />
+      </Element>
     </Router>
   </Provider>,
   document.getElementById('app')
