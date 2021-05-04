bbe26dbb884f8ea9584c8a9cf2738df763fecfe7
Sung Lee <sungyonglee414@gmail.com>
2021-05-04 12:50:37 -0700

    style: animation added for intro.

--- client/components/main/Intro.js
+++ client/components/main/Intro.js
@@ -5,31 +5,38 @@ const Intro = () => {
     <div className="intro">
       <img src="/ironmanAboutUs.jpeg" alt="introImg" className="introImg" />
       <div className="about-us">
-        <div className="about-us-text-container">
-          at
-          <strong
-            style={{
-              fontSize: '2.5rem',
-              fontStyle: 'italic',
-              letterSpacing: '0.1rem'
-            }}
-          >
-            stark industries
-          </strong>
+        <div className="block">
+          <div className="about-us-text-container">
+            at
+            <strong
+              style={{
+                fontSize: '2.5rem',
+                fontStyle: 'italic',
+                letterSpacing: '0.1rem',
+                marginRight: '0.5rem'
+              }}
+            >
+              stark industries
+            </strong>
+          </div>
         </div>
 
-        <div className="about-us-text-container">
-          we design, manufacture, and deliver
+        <div className="block">
+          <div className="about-us-text-container">
+            we design, manufacture, and deliver
+          </div>
         </div>
 
-        <div className="about-us-text-container">
-          advanced
-          <strong
-            className="pseudo"
-            style={{letterSpacing: '0.1rem', fontSize: '2.5rem'}}
-          >
-            pseudo-armor.
-          </strong>
+        <div className="block">
+          <div className="about-us-text-container">
+            advanced
+            <strong
+              className="pseudo"
+              style={{letterSpacing: '0.1rem', fontSize: '2.5rem'}}
+            >
+              pseudo-armor.
+            </strong>
+          </div>
         </div>
       </div>
     </div>
