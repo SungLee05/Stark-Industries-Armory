a175afd5142cb7a610e07a2380a0c2b8bd07ed6d
Sung Lee <sungyonglee414@gmail.com>
2021-05-04 18:39:50 -0700

    style: scroll animation for landing page added.

--- client/components/main/Directory.js
+++ client/components/main/Directory.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {Link} from 'react-router-dom'
+import {gsap} from 'gsap'
+import {ScrollTrigger} from 'gsap/ScrollTrigger'
 
 const Directory = () => {
+  gsap.registerPlugin(ScrollTrigger)
+
+  useEffect(() => {
+    let timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#robotic-link-text',
+        toggleActions: 'restart none none none',
+        start: 'top bottom'
+      }
+    })
+    timeline.from('#robotic-link-text', {
+      x: -1000,
+      duration: 3
+    })
+  })
+
   return (
     <div className="directory">
       <div className="wrap">
