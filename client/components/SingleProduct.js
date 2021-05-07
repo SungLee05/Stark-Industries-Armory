63af3d98a48f018436d591c54333081b41c3dedc
Sung Lee <sungyonglee414@gmail.com>
2021-05-07 10:28:13 -0700

    feat/style: tilt feature added to single product image section. single product image complete.;

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -20,59 +20,67 @@ const SingleProduct = props => {
   }, [])
 
   return (
-    <div className="single-product-container">
-      <div className="product-img-container">
-        <Tilt
-          options={{
-            max: 30,
-            speed: 2000,
-            transition: true,
-            easing: 'cubic-bezier(.03,.98,.52,.99)'
-          }}
-          className="product-img-card"
-          style={{padding: 0}}
-        >
-          <img
-            src={
-              singleProduct.singleInfoImageUrl
-                ? singleProduct.singleInfoImageUrl
-                : singleProduct.imageUrl
-            }
-            className="single-product-img"
-          />
-        </Tilt>
+    <>
+      <div id="jarvis-container">
+        <img src="/jarvisGIF2.gif" alt="jarvis" id="jarvis" />
       </div>
+      <div className="single-product-container">
+        <div className="product-img-container">
+          <Tilt
+            options={{
+              max: 30,
+              speed: 1000,
+              transition: true,
+              perspective: 500,
+              easing: 'cubic-bezier(.03,.98,.52,.99)'
+            }}
+            className="product-img-card"
+            style={{padding: 0}}
+          >
+            <img
+              src={
+                singleProduct.singleInfoImageUrl
+                  ? singleProduct.singleInfoImageUrl
+                  : singleProduct.imageUrl
+              }
+              className="single-product-img"
+            />
+          </Tilt>
+        </div>
 
-      <div>
-        <div className="single-product-info-container">
-          <div className="single-product-name">{singleProduct.name}</div>
-          <div className="single-product-desc">
-            Description: {singleProduct.description}
+        <div className="single-product-glass">
+          <div className="text-container">
+            <div className="single-product-info-container">
+              <div className="single-product-name">{singleProduct.name}</div>
+              <div className="single-product-desc">
+                Description: {singleProduct.description}
+              </div>
+            </div>
+            <div className="single-product-price">
+              Price: ${singleProduct.price}
+            </div>
+
+            {!userId ? (
+              <button
+                className="order-btn"
+                type="submit"
+                onClick={() => addProductToGuestCart(singleProduct)}
+              >
+                ORDER
+              </button>
+            ) : (
+              <button
+                className="order-btn"
+                type="submit"
+                onClick={() => addProductToUserCart(singleProduct, userId)}
+              >
+                ORDER
+              </button>
+            )}
           </div>
         </div>
-        <div className="single-product-price">
-          Price: ${singleProduct.price}
-        </div>
-
-        {!userId ? (
-          <button
-            className="order-btn"
-            type="submit"
-            onClick={() => addProductToGuestCart(singleProduct)}
-          >
-            ORDER
-          </button>
-        ) : (
-          <button
-            className="order-btn"
-            type="submit"
-            onClick={() => addProductToUserCart(singleProduct, userId)}
-          >
-            ORDER
-          </button>
-        )}
       </div>
-    </div>
+    </>
   )
 }
 
