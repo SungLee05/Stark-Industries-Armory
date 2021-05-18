059405fddf32d46e8169aaa3f5b0e404ad5c35d2
Sung Lee <sungyonglee414@gmail.com>
2021-05-17 19:18:26 -0700

    feat/style: design for order history slots added. order history now includes shipping address info along with tracking status bar.

--- client/components/OrderHistory.js
+++ client/components/OrderHistory.js
@@ -3,10 +3,21 @@ import {connect, useDispatch} from 'react-redux'
 import dateFormat from 'dateformat'
 import {getOrderHistory} from '../store/orderHistory'
 import accounting from 'accounting'
+const faker = require('faker')
 
 const OrderHistory = props => {
   const dispatch = useDispatch()
   const {history} = props
+  const streetName = faker.address.streetAddress()
+  const cityName = faker.address.cityName()
+  const state = faker.address.stateAbbr()
+  const zipCode = faker.address.zipCode()
+
+  const estimatedArrivalDate = (date, days) => {
+    let result = new Date(date)
+    result.setDate(result.getDate() + days)
+    return result
+  }
 
   useEffect(
     () => {
@@ -24,71 +35,115 @@ const OrderHistory = props => {
             <div>You have not purchased any of our products.</div>
           ) : (
             <div>
-              {history.map(order => {
-                return (
-                  <div key={order.id}>
-                    <br />
+              {history
+                .slice(0)
+                .reverse()
+                .map(order => {
+                  return (
+                    <div key={order.id}>
+                      <br />
 
-                    <div className="order-history-header-wrapper">
-                      <div>
-                        Order Placed: {dateFormat(order.updatedAt, 'fullDate')}
+                      <div className="order-history-header-wrapper">
+                        <div style={{marginLeft: '1rem'}}>
+                          Order Placed:{' '}
+                          {dateFormat(order.updatedAt, 'mmm d, yyyy')}
+                        </div>
+
+                        <div style={{marginRight: '1rem'}}>
+                          Total:{' '}
+                          {accounting.formatMoney(
+                            order.products.reduce(
+                              (acc, product) =>
+                                acc +
+                                product.price * product.orderHistory.quantity,
+                              0
+                            )
+                          )}
+                        </div>
                       </div>
 
                       <div>
-                        Total:
-                        {accounting.formatMoney(
-                          order.products.reduce(
-                            (acc, product) =>
-                              acc +
-                              product.price * product.orderHistory.quantity,
-                            0
+                        {order.products.map(product => {
+                          return (
+                            <div
+                              key={product.id}
+                              className="order-history-glass-container"
+                            >
+                              <div className="order-history-subheader-wrapper">
+                                <div
+                                  style={{marginLeft: '9rem', width: '10rem'}}
+                                >
+                                  {product.name}
+                                </div>
+                                <div style={{marginLeft: '10.25rem'}}>
+                                  Shipping Info
+                                </div>
+                                <div style={{marginLeft: '14rem'}}>
+                                  Package Status
+                                </div>
+                              </div>
+
+                              <div className="order-history-content-wrapper">
+                                <div className="order-history-img-wrapper">
+                                  <img src={product.imageUrl} height="170" />
+                                </div>
+                                <div className="order-history-qty-price-wrapper">
+                                  <div>
+                                    Qty: {product.orderHistory.quantity}
+                                  </div>
+                                  <div>Price: ${product.price}</div>
+                                </div>
+
+                                <div className="order-history-address-wrapper">
+                                  <div>{streetName}</div>
+                                  <div>
+                                    {cityName + ', ' + state + ' ' + zipCode}
+                                  </div>
+                                </div>
+
+                                <div className="order-history-status-wrapper">
+                                  <div className="track-status-container">
+                                    <div className="track-status-wrapper">
+                                      <div className="track-status-confirmed">
+                                        Confirmed
+                                      </div>
+                                      <div className="track-status-shipped">
+                                        Shipped
+                                      </div>
+                                      <div className="track-status-delivered">
+                                        Delivered
+                                      </div>
+                                    </div>
+
+                                    <div className="track-status-wrapper">
+                                      <div className="track-status-box-left" />
+                                      <div className="track-status-box-middle" />
+                                      <div className="track-status-box-right" />
+                                    </div>
+                                  </div>
+
+                                  <div
+                                    style={{
+                                      fontSize: '0.8rem',
+                                      fontWeight: '400',
+                                      textAlign: 'center'
+                                    }}
+                                  >
+                                    Estimated Arrival:{' '}
+                                    {dateFormat(
+                                      estimatedArrivalDate(order.updatedAt, 5),
+                                      'ddd, mmm d'
+                                    )}
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
                           )
-                        )}
+                        })}
                       </div>
                     </div>
-
-                    <div>
-                      {order.products.map(product => {
-                        return (
-                          <div key={product.id}>
-                            <div className="order-history-subheader-wrapper">
-                              <div
-                                style={{marginLeft: '7.75rem', width: '10rem'}}
-                              >
-                                {product.name}
-                              </div>
-                              <div style={{marginLeft: '11.25rem'}}>
-                                Shipping Info
-                              </div>
-                              <div style={{marginLeft: '15rem'}}>
-                                Package Status
-                              </div>
-                            </div>
-
-                            <div className="order-history-content-wrapper">
-                              <div className="order-history-img-wrapper">
-                                <img src={product.imageUrl} height="170" />
-                              </div>
-                              <div className="order-history-qty-price-wrapper">
-                                <div>Qty: {product.orderHistory.quantity}</div>
-                                <div>Price: ${product.price}</div>
-                              </div>
-
-                              <div className="order-history-address-wrapper">
-                                <div>ADDRESS HERE</div>
-                              </div>
-
-                              <div className="order-history-status-wrapper">
-                                <div>PACKAGE STATUS HERE</div>
-                              </div>
-                            </div>
-                          </div>
-                        )
-                      })}
-                    </div>
-                  </div>
-                )
-              })}
+                  )
+                })}
             </div>
           )}
         </div>
