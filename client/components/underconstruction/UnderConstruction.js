63bdbb5413b7c5af44bd0d669e41751cd204f467
Sung Lee <sungyonglee414@gmail.com>
2021-05-19 09:49:31 -0700

    feat/style: under constructions page remastered.

--- client/components/underconstruction/UnderConstruction.js
+++ client/components/underconstruction/UnderConstruction.js
@@ -3,10 +3,17 @@ import React from 'react'
 const UnderConstruction = () => {
   return (
     <div className="under-construction-container">
+      <div className="UC-background-img-container">
+        <img
+          src="/images/broken-helmet.jpeg"
+          alt="broken-helmet"
+          className="UC-background-img"
+        />
+      </div>
       <div className="under-construction-wrapper">
         <h1>
-          SORRY! <br />
-          PAGE UNDER CONSTRUCTION :(
+          We apologize for the inconvenience. <br />
+          This page is currently <strong>under construction.</strong>
         </h1>
       </div>
     </div>
