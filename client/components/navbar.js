d0053e8cac1a0252893e97184a1321039b792d2b
Sung Lee <sungyonglee414@gmail.com>
2021-05-21 10:44:25 -0700

    chore: unnecessary import removed from navbarjs

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -1,5 +1,5 @@
 /* eslint-disable complexity */
-import React, {useEffect, useState} from 'react'
+import React, {useEffect} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
