dcbb7c7a13ed145f36a5ce8469d6573796c3b408
Sung Lee <sungyonglee414@gmail.com>
2021-05-20 18:39:36 -0700

    security: userAuth added to viewing order history

--- server/auth/userAuth.js
+++ server/auth/userAuth.js
@@ -2,7 +2,7 @@ const userAuth = (req, res, next) => {
   if (!req.user || !req.user.id) {
     return res
       .status(401)
-      .send('Admin Access Required: Not authorized to view this content')
+      .send('User Access Required: Not authorized to view this content')
   } else {
     next()
   }
